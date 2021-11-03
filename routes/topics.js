ac58a0ff0280810dc8338a6cabb13e9bc1e8c150
corshi.dan <corshi.dan@gmail.com>
2021-11-03 13:54:26 +0000

    _

--- routes/topics.js
+++ routes/topics.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTopics } = require('../models/topics');
+const { getAllTopics, getAllTopicsByDate } = require('../models/topics');
 router.get('/', async (req, res) => {
+	const { date } = req.query;
 	const topics = await getAllTopics();
+	if (date) {
+		try {
+			const topics = await getAllTopicsByDate(date);
+			res.json({
+				success: true,
+				message: 'Topics by date',
+				payload: topics,
+			});
+			return;
+		} catch (err) {
+			console.error(err);
+		}
+	}
 	res.json({
 		success: true,
 		message: 'Here are all the topics',
