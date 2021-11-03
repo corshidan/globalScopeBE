1cf6908f1cc51fb98ddc78eb62bdd9e3633b86a7
corshi.dan <corshi.dan@gmail.com>
2021-11-03 12:22:06 +0000

    _

--- models/topics.js
+++ models/topics.js
@@ -2,14 +2,20 @@ const { query } = require('../db/index');
 const removeTimeFromDate = require('../functions/dateFormat');
 
 async function getAllTopics() {
-	const sqlString = `SELECT * FROM topics;`;
+	const sqlString = `SELECT topics,created FROM reflections;`;
 	try {
 		const { rows: data, command } = await query(sqlString);
 		console.log(command);
-		return data.map((topic) => {
-			topic.datecreated = removeTimeFromDate(topic.datecreated.toISOString());
-			return topic;
+		const topicsArray = [];
+		data.map((item) => {
+			topicsArray.push(
+				...item.topics.map((topics) => {
+					const { topic, rating } = JSON.parse(topics);
+					return { topic, rating, date: removeTimeFromDate(item.created.toISOString()) };
+				})
+			);
 		});
+		return topicsArray;
 	} catch (err) {
 		console.error(err);
 	}
