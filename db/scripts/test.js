79e2ff9c4cf2646b2d72be68570ad9f45d364be1
corshi.dan <corshi.dan@gmail.com>
2022-01-10 18:14:39 +0000

    more options for the test scritp

--- db/scripts/test.js
+++ db/scripts/test.js
@@ -1,4 +1,5 @@
 const { query } = require('../index');
+const bcrypt = require('bcrypt');
 
 async function alterTable() {
 	const sqlString = `ALTER TABLE bootcampers ALTER COLUMN password TYPE VARCHAR(150) ;`;
@@ -10,4 +11,30 @@ async function alterTable() {
 		console.log(error);
 	}
 }
-alterTable();
+// alterTable();
+
+async function updatePasswords() {
+	const sqlString = `UPDATE  bootcampers SET password=$1 WHERE  id=6 ;`;
+	const password = 'socadmin';
+	const hashedPassword = await bcrypt.hash(password, 10);
+	try {
+		const res = await query(sqlString, [hashedPassword]);
+		console.log(res.command);
+	} catch (error) {
+		console.log(error);
+	}
+}
+// updatePasswords();
+
+async function deleteEntries() {
+	const sqlString = `DELETE FROM  bootcampers WHERE  id=5 ;`;
+	try {
+		const res = await query(sqlString);
+		console.log(res.command);
+	} catch (error) {
+		console.log(error);
+	}
+}
+// deleteEntries();
+
+const basicPass = ['waseem', 'eleanor', 'filipe', 'socadmin', 'daniel'];
